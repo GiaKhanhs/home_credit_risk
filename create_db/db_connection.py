a0c8ef3b7462a2e20f563e18d79c726532542bfa
Stephan <giakhanh2091@gmail.com>
2026-03-20 17:11:25 +0700

    Create engine for EDA using

--- create_db/db_connection.py
+++ create_db/db_connection.py
@@ -1,12 +1,25 @@
 import psycopg2
 
+from sqlalchemy import create_engine
+
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_USER = "postgres"
+DB_PASSWORD = "209202"
+DB_NAME = "postgres"
+
 def get_connection(database = "postgres"):
     return psycopg2.connect(
-        host = "localhost",
-        port = 5432,
-        database = database,
-        user = "postgres",
-        password = "209202"
+        host=DB_HOST,
+        port=DB_PORT,
+        database=database,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
+def get_engine(database = "postgres"):
+    return create_engine(
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{database}"
     )
 
 if __name__ == "__main__":
