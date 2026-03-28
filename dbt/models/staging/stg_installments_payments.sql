864680d8bc18fa96f820f32677680d45deca317f
Stephan <giakhanh2091@gmail.com>
2026-03-28 14:58:44 +0700

    Fix CAST type

--- dbt/models/staging/stg_installments_payments.sql
+++ dbt/models/staging/stg_installments_payments.sql
@@ -4,14 +4,17 @@ WITH stg_installments_payments__source AS (
 )
     , stg_installments_payments__redefined AS (
         SELECT
-            CAST(sk_id_prev AS BIGINT)                   AS sk_id_prev,
-            CAST(sk_id_curr AS BIGINT)                   AS sk_id_curr,
-            CAST(num_instalment_version AS INTEGER)      AS num_instalment_version,
-            CAST(num_instalment_number AS INTEGER)       AS num_instalment_number,
-            CAST(days_instalment AS INTEGER)             AS days_instalment,
-            CAST(days_entry_payment AS INTEGER)          AS days_entry_payment,
-            CAST(amt_instalment AS NUMERIC)              AS amt_instalment,
-            CAST(amt_payment AS NUMERIC)                 AS amt_payment
+            CAST(sk_id_prev AS BIGINT)                                      AS sk_id_prev,
+            CAST(sk_id_curr AS BIGINT)                                      AS sk_id_curr,
+            
+            CAST(CAST(num_instalment_version AS NUMERIC) AS INTEGER)        AS num_instalment_version,
+            CAST(num_instalment_number AS INTEGER)                          AS num_instalment_number,
+
+            CAST(CAST(days_instalment AS NUMERIC) AS INTEGER)               AS days_instalment,
+            CAST(CAST(days_entry_payment AS NUMERIC) AS INTEGER)            AS days_entry_payment,
+
+            CAST(amt_instalment AS NUMERIC)                                 AS amt_instalment,
+            CAST(amt_payment AS NUMERIC)                                    AS amt_payment
         FROM stg_installments_payments__source
     )
 
